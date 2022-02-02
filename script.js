8416da507b4110aea9f9a2aa49d12aef90be4a68
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-02-01 23:11:19 -0800

    Add grid with variable rows and columns

---
+++ script.js
@@ -0,0 +1,23 @@
+const gridContainer = document.querySelector('.grid-container');
+
+const rows = 16;
+const cols = 16;
+
+for (let i = 0; i < rows; i++)
+{
+    const row = document.createElement('div');
+    row.style.display = "flex";
+    row.style.flexDirection = "column";
+    row.style.gap = "0";
+    for (let j = 0; j < cols; j++)
+    {
+        const cel = document.createElement('div');
+        cel.style.width = '24px';
+        cel.style.height = '24px';
+        cel.style.backgroundColor = "lightblue";
+        cel.style.border = "1px solid black";
+        cel.style.borderRadius = "4px";
+        row.appendChild(cel);
+    }
+    gridContainer.appendChild(row);
+}
